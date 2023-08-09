3b06d197f9d82cf5553cac0444f00ed4a6f30c58
Ayush Pandit <ayushtripathi.india@gmial.com>
2023-08-09 09:06:12 +0530

    new codes and files added

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    # check to see if logingin
+    if request.method == 'POST':
+        username= request.POST['username']
+        password= request.POST['password']
+        # Authenticate
+        user= authenticate(request, username=username, password= password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged")
+            return redirect('home')
+        else:
+            messages.error(request, "Ther's an error, please try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
