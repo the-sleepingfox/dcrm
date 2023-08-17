57713cb2c1a4e3309f477feef4a296b1a734e6d9
Ayush Pandit <ayushtripathi.india@gmial.com>
2023-08-17 12:04:46 +0530

    all vercel settings reverted deployement aborted

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*st%awuoit$3#ixzkf2ax6td$b+6s81@f^5e!@7m5z^t)gkm*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,12 +127,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# adding some codes for vercel deployment
-
-import os
-STATIC_URL= 'static/'
-STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-# end
